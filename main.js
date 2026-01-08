54ed715ca10548e6276cd2353d3047157cbc9033
iabdo06450645-pixel <iabdo06450645@gmail.com>
2026-01-09 02:20:42 +0400

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ btn.onclick=function(){
     btn.hidden=true;
     num.hidden=true;
      const el = document.createElement('img');
-                    el.src = `https://github.com/iabdo06450645-pixel/zad/blob/main/${num.value}.png?raw=true`;
+                    el.src = `https://github.com/iabdo06450645-pixel/zadtot/blob/main/${num.value}.png?raw=true`;
                     
                     cont.appendChild(el)
 }
